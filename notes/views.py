f92c051bd7c362c79deff6455af1c0e053f427aa
Edward Manda <lilkups@yahoo.com>
2018-06-26 18:02:32 -0500

    made it through displaying notes and bookmarks in the view

--- notes/views.py
+++ notes/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-
+from . models import Note
 # Create your views here.
 
 
 def index(request):
-    context = {}
+    context = {"notes": Note.objects.all()}
     return render(request, 'notes/index.html', context)
