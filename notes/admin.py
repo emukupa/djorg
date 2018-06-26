025d2ab301528b432366dbbd4964210fa1e94bc2
Edward Manda <lilkups@yahoo.com>
2018-06-26 17:45:13 -0500

    bookmark and notes views added

--- notes/admin.py
+++ notes/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
 from .models import Note, PersonalNote
 # Register your models here.
-admin.site.register(Note)
-admin.site.register(PersonalNote)
+admin.site.register((Note, PersonalNote,))
