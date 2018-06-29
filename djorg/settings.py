b118680ea3ba66c3a7da799679cc62120854c776
Edward Manda <lilkups@yahoo.com>
2018-06-29 12:20:58 -0500

    static root added

--- djorg/settings.py
+++ djorg/settings.py
@@ -136,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # http://www.django-rest-framework.org/
 REST_FRAMEWORK = {
