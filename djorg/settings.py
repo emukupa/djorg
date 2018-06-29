08e74775a314c652e44933f0cc43b393fce640a0
Edward Manda <lilkups@yahoo.com>
2018-06-29 11:42:18 -0500

    check in

--- djorg/settings.py
+++ djorg/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-'''
+
 # DATABASES['default'] = dj_database_url.config(default='postgres://...')
 DATABASES = {
     'default': config(
@@ -98,7 +98,7 @@ DATABASES = {
         cast=db_url
     )
 }
-
+'''
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
